e87afb60a5a091fca27d01181f9a9f7a84b5b83b
Nigel Horne <njh@bandsman.co.uk>
2025-01-05 16:38:52 -0500

    Release 0.02

--- lib/CPAN/UnsupportedFinder.pm
+++ lib/CPAN/UnsupportedFinder.pm
@@ -19,11 +19,11 @@ CPAN::UnsupportedFinder analyzes CPAN modules for test results and maintenance s
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
